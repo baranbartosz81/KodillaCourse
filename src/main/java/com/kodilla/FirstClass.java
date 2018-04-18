5fcf575afed0d1004916c84e28a6335f78893d1a
baranbartosz81 <bartoszbaran@wp.pl>
2018-04-18 20:07:55 +0200

    initial commit

---
+++ src/main/java/com/kodilla/FirstClass.java
@@ -0,0 +1,15 @@
+package com.kodilla;
+
+public class FirstClass {
+
+    public void someMethod() {
+        System.out.println("It works!");
+    }
+
+
+    public static void main(String args[]){
+
+        FirstClass object=new FirstClass();
+        object.someMethod();
+        }
+}
